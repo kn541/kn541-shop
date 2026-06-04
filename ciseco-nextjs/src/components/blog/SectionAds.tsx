fd61a21ade64969aba3efdcbcbbf24c0a604d08c
kn541 <kn541dev@gmail.com>
2026-06-05 07:45:19 +0900

    chore: ciseco 데모 잔재 정리 — about/blog/contact/subscription/account-billing/home-2/coming-soon/navigation/Header2
    ciseco 데모 잔재 21개 파일 정리 — about/blog/contact/subscription/account-billing/home-2/coming-soon/navigation/Header2 (참조 0건 확인, 동작 변경 0)

--- ciseco-nextjs/src/components/blog/SectionAds.tsx
+++ ciseco-nextjs/src/components/blog/SectionAds.tsx
@@ -1,18 +1,2 @@
-import imgAds from '@/images/ads.png'
-import clsx from 'clsx'
-import Image from 'next/image'
-import { FC } from 'react'
-
-interface SectionAdsProps {
-  className?: string
-}
-
-const SectionAds: FC<SectionAdsProps> = ({ className }) => {
-  return (
-    <div className={clsx(className)}>
-      <Image alt="ads" className="w-full" src={imgAds} width={imgAds.width} height={imgAds.height} />
-    </div>
-  )
-}
-
-export default SectionAds
+// blog 코드 전용 — blog pages notFound() 전환으로 참조 0건
+export {}
